336f7a6fe972fe0014adea5e43026d3f072e9311
s-baron <savannah@getstride.com>
2017-10-10 15:15:46 -0700

    Updated files

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ async function fetchSentryUrl(env: string, appName: string) {
     if (env !== 'dev') {
         const cred = await fetchAppCred(env, appName, `sentry.url`, '');
         if (cred === '') {
-            return undefined
+            return undefined;
         }
         return cred;
     }
@@ -17,33 +17,45 @@ export interface RavenClient {
     captureMessagePromise: (msg: string, extra?: any) => Promise<string>;
 }
 
-export async function init(env: string, appName: string, extraOptions?: Object): Promise<RavenClient> {
+export async function init(
+    env: string,
+    appName: string,
+    extraOptions?: Object
+): Promise<RavenClient> {
     const sentryUrl = await fetchSentryUrl(env, appName);
 
-    Raven.config(sentryUrl,
-        Object.assign({
-            captureUnhandledRejections: true,
-            environment: appName,
-            release: process.env.BUILD_NUM,
-            tags: {
-                env,
-                'build_hash': process.env.BUILD_HASH,
-                'build_time': process.env.BUILD_TIME,
-            }
-        }, extraOptions)).install();
+    Raven.config(
+        sentryUrl,
+        Object.assign(
+            {
+                captureUnhandledRejections: true,
+                environment: appName,
+                release: process.env.BUILD_NUM,
+                tags: {
+                    env,
+                    build_hash: process.env.BUILD_HASH,
+                    build_time: process.env.BUILD_TIME,
+                },
+            },
+            extraOptions
+        )
+    ).install();
 
-    Raven.captureExceptionPromise = (err: any, tags?: any) => new Promise<string>((resolve, reject) =>
-        Raven.captureException(err, { tags }, (sendErr: any, eventId: string) => {
-            if (sendErr) reject({ sendErr, eventId })
-            resolve(eventId)
-        }));
+    Raven.captureExceptionPromise = (err: any, tags?: any) =>
+        new Promise<string>((resolve, reject) =>
+            Raven.captureException(err, { tags }, (sendErr: any, eventId: string) => {
+                if (sendErr) reject({ sendErr, eventId });
+                resolve(eventId);
+            })
+        );
 
-    Raven.captureMessagePromise = (msg: string, tags?: any) => new Promise<string>((resolve, reject) =>
-        Raven.captureMessage(msg, { tags }, (sendErr: any, eventId: string) => {
-            if (sendErr) reject({ sendErr, eventId })
-            resolve(eventId)
-        }));
+    Raven.captureMessagePromise = (msg: string, tags?: any) =>
+        new Promise<string>((resolve, reject) =>
+            Raven.captureMessage(msg, { tags }, (sendErr: any, eventId: string) => {
+                if (sendErr) reject({ sendErr, eventId });
+                resolve(eventId);
+            })
+        );
 
     return Raven as RavenClient;
 }
-
--- test/test.ts
+++ test/test.ts
@@ -7,18 +7,20 @@ import { init } from '../src';
 import * as AWS from 'aws-sdk';
 AWS.config.region = 'us-west-2';
 
-describe("basic test", function () {
-    it("connects", async function () {
-        this.timeout(10000);
+describe('basic test', function() {
+    it('connects', async function() {
         process.env.BUILD_HASH = 'test';
 
-        let ravenClient = await init('test', 'test');
-        assert.isDefined(ravenClient, "got a raven client");
+        const ravenClient = await init('test', 'test');
+        assert.isDefined(ravenClient, 'got a raven client');
 
         const result = await ravenClient.captureMessagePromise('test');
-        assert.typeOf(result, 'string')
+        assert.typeOf(result, 'string');
 
-        const result2 = await ravenClient.captureExceptionPromise(new Error('test'), { foo: 1, tags: { bar: 2 } });
-        assert.typeOf(result2, 'string')
-    })
+        const result2 = await ravenClient.captureExceptionPromise(new Error('test'), {
+            foo: 1,
+            tags: { bar: 2 },
+        });
+        assert.typeOf(result2, 'string');
+    });
 });
